d080cca0ad925d835b5483cb3b3cb09bb9a347f3
Chris Cole <goosemanjack@gmail.com>
2017-09-18 13:42:30 -0700

    Pushing index file for Github Pages

--- src/Measurement/MeasurementTechniques/index.js
+++
@@ -1,3 +0,0 @@
-export { default as IntersectionObserver } from './IntersectionObserver';
-export { default as IntersectionObserverPolyfill } from './IntersectionObserverPolyfill';
-export { default as BaseTechnique } from './BaseTechnique';
